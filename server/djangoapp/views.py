a4ac6835eada509bb71a99d272536eb7f276f386
leviatan1026 <os546029@gmail.com>
2026-05-04 19:16:26 -0400

    Update views, urls, and frontend components

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -82,7 +82,9 @@ def get_dealer_details(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchDealer/" + str(dealer_id)
         dealership = get_request(endpoint)
-        return JsonResponse({"status": 200, "dealer": dealership})
+        return JsonResponse({"status": 200, "dealer": [dealership]})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
 
 def get_dealer_reviews(request, dealer_id):
     if(dealer_id):
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,23 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-//  IMPORTS
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";
-
-// (si tienes Home en React lo importas, si no Django lo maneja)
-// import Home from "./components/Home/Home";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview";
 
 function App() {
   return (
     <Routes>
-
-      {/* 🔥 LOGIN */}
       <Route path="/login" element={<LoginPanel />} />
-
-      {/* 🔥 REGISTER */}
       <Route path="/register" element={<Register />} />
-
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
