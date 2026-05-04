33c8a1baeffa772aeca2cefd9a69358477212bf0
leviatan1026 <os546029@gmail.com>
2026-05-04 17:31:35 -0400

    Add CarMake/CarModel models, get_cars view, and fix CSRF trusted origins

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -11,6 +11,7 @@ import logging
 import json
 
 from .populate import initiate
+from .models import CarMake, CarModel
 
 # Logger
 logger = logging.getLogger(__name__)
@@ -99,6 +100,17 @@ def registration(request):
             "userName": username,
             "error": "Already Registered"
         })
+        
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -14,11 +14,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'os546029-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'os546029-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'os546029-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://os546029-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://os546029-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://os546029-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
